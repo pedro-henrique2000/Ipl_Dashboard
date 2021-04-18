8ac5bc50cab3577e405c838fe64ee976ee67bfe5
phb2000 <p.becker2000@hotmail.com>
2021-04-18 17:21:49 -0300

    first commit

---
+++ src/main/java/io/projects/demo/data/MatchDataProcessor.java
@@ -0,0 +1,42 @@
+package io.projects.demo.data;
+
+import io.projects.demo.model.Match;
+import org.springframework.batch.item.ItemProcessor;
+
+import java.time.LocalDate;
+
+public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
+
+
+    @Override
+    public Match process(final MatchInput matchInput) throws Exception {
+        Match match = new Match();
+
+        match.setId(Long.parseLong(matchInput.getId()));
+        match.setCity(matchInput.getCity());
+        match.setDate(LocalDate.parse(matchInput.getDate()));
+        match.setPlayerOfMatch(matchInput.getPlayer_of_match());
+
+        String firstInningsTeam, secondInningsTeam;
+
+        if("bat".equals(matchInput.getToss_decision())) {
+            firstInningsTeam = matchInput.getToss_winner();
+            secondInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1())
+                    ? matchInput.getTeam2() : matchInput.getTeam1();
+        } else {
+            secondInningsTeam = matchInput.getToss_decision();
+            firstInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1())
+                    ? matchInput.getTeam2() : matchInput.getTeam1();
+        }
+
+        match.setTeam1(firstInningsTeam);
+        match.setTeam2(secondInningsTeam);
+        match.setTossWinner(matchInput.getToss_winner());
+        match.setTossDecision(matchInput.getToss_decision());
+        match.setResult(match.getResult());
+        match.setUmpire1(matchInput.getUmpire1());
+        match.setUmpire2(matchInput.getUmpire2());
+
+        return match;
+    }
+}
---
+++ src/test/java/io/projects/demo/IplDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.projects.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
