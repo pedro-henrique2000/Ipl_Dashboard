4e8f551c7b9a26e43988300ff13b856d6d0361df
phb2000 <p.becker2000@hotmail.com>
2021-04-29 18:20:07 -0300

    team entity

--- src/main/java/io/projects/demo/data/JobCompletionNotificationListener.java
+++ src/main/java/io/projects/demo/data/JobCompletionNotificationListener.java
@@ -1,34 +1,63 @@
 package io.projects.demo.data;
 
+import io.projects.demo.model.Team;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import javax.persistence.EntityManager;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Component
 public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
 
     private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
 
-    private final JdbcTemplate jdbcTemplate;
+    private final EntityManager em;
 
     @Autowired
-    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public JobCompletionNotificationListener(EntityManager em) {
+        this.em = em;
     }
 
     @Override
+    @Transactional
     public void afterJob(JobExecution jobExecution) {
-        if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
+        if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
             log.info("!!! JOB FINISHED! Time to verify the results");
+            Map<String, Team> teamData = new HashMap<>();
 
-            jdbcTemplate.query("SELECT team1, team2, date FROM match",
-                    (rs, row) -> "Team 1 " + rs.getString(1) + " Team 2 " + rs.getString(2) + " Date " + rs.getString(3)
-            ).forEach(str -> System.out.println(str));
+            List<Object[]> resultsTeam1 = em.createQuery("select distinct m.team1, count(*) from Match m group by m.team1")
+                    .getResultList();
+
+            resultsTeam1.stream().map(e -> new Team((String) e[0], (long) e[1]))
+                    .forEach(team -> teamData.put(team.getTeamName(), team));
+
+            List<Object[]> resultsTeam2 = em.createQuery("select distinct m.team2, count(*) from Match m group by m.team2")
+                    .getResultList();
+
+            resultsTeam2.stream().forEach(e -> {
+                Team team = teamData.get((String) e[0]);
+                if(team != null)
+                    team.setTotalMatches(team.getTotalMatches() + (long) e[1]);
+            });
+
+            em.createQuery("select m.matchWinner, count(*) from Match m group by m.matchWinner", Object[].class).getResultList().forEach(e -> {
+                Team team = teamData.get((String) e[0]);
+                if(team != null)
+                    team.setTotalWins((long) e[1]);
+            });
+
+            teamData.values().forEach(team -> em.persist(team));
+            teamData.values().forEach(team -> System.out.println(team));
+
+
         }
     }
 }
